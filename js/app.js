8157e968c9b1b6377b412c59ccef5089efbd4f9d
grahamjones97 <graham.jones97@hotmail.com>
2022-04-04 18:28:27 +0100

    Added JS to featured stays with Anna and Rebecca

--- js/app.js
+++ js/app.js
@@ -2,11 +2,15 @@ const formButton = document.getElementById("formButton");
 console.log(formButton);
 formButton.onclick = function () {
   //   console.log("test");
-  // window.location.href = "./FeaturedStays.html";
   const checkInDate = document.getElementById("checkIn");
   const checkOutDate = document.getElementById("checkOut");
   const noOfPeople = document.getElementById("noOfPeople");
   console.log("Check out date: " + noOfPeople.value);
   console.log("Check in date: " + checkInDate.value);
   console.log("Check out date: " + checkOutDate.value);
+  localStorage.setItem("noOfPeople", noOfPeople.value);
+  localStorage.setItem("checkInDate", checkInDate.value);
+  localStorage.setItem("checkOutDate", checkOutDate.value);
+
+  window.location.href = "./FeaturedStays.html";
 };
