8fb0f180d7da1d6234bac5210aa0d5b673ab93fa
grahamjones97 <graham.jones97@hotmail.com>
2022-04-04 16:19:07 +0100

    Test

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-'use strict'
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ' + className);
-    
-});
-
-
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
