233de2b004e11846afd79dfcb51e818b9692a232
grahamjones97 <graham.jones97@hotmail.com>
2022-04-04 17:49:26 +0100

    removing slider

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+const formButton = document.getElementById("formButton");
+console.log(formButton);
+formButton.onclick = function () {
+  //   console.log("test");
+  // window.location.href = "./FeaturedStays.html";
+  const checkInDate = document.getElementById("checkIn");
+  const checkOutDate = document.getElementById("checkOut");
+  const noOfPeople = document.getElementById("noOfPeople");
+  console.log("Check out date: " + noOfPeople.value);
+  console.log("Check in date: " + checkInDate.value);
+  console.log("Check out date: " + checkOutDate.value);
+};
