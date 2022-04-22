80ca7a84b1c1ebea4f2e5adedab08a9195580bb3
RebeccaFrancey <francey-r@ulster.ac.uk>
2022-04-22 10:58:01 +0100

    added price option to form homepage

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@ formButton.onclick = function () {
   const checkInDate = document.getElementById("checkIn");
   const checkOutDate = document.getElementById("checkOut");
   const noOfPeople = document.getElementById("noOfPeople");
+  const price = document.getElementById("price");
   console.log("Check out date: " + noOfPeople.value);
   console.log("Check in date: " + checkInDate.value);
   console.log("Check out date: " + checkOutDate.value);
+  console.log("price: " + price.value);
   localStorage.setItem("noOfPeople", noOfPeople.value);
   localStorage.setItem("checkInDate", checkInDate.value);
   localStorage.setItem("checkOutDate", checkOutDate.value);
+  localStorage.setItem("price", price.value);
 
   window.location.href = "./FindYourStay.html";
 };
--- js/findyourstay.js
+++ js/findyourstay.js
@@ -2,7 +2,9 @@ document.body.onload = function () {
     var noOfPeople = localStorage.getItem("noOfPeople");
     var checkInDate = localStorage.getItem("checkInDate");
     var checkOutDate = localStorage.getItem("checkOutDate");
+    var price = localStorage.getItem("price");
     console.log(noOfPeople);
     console.log(checkInDate);
     console.log(checkOutDate);
+    console.log(price);
   };
