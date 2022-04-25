3cafd98b424d0b5c8cb6cf384c1d4135835346aa
grahamjones97 <graham.jones97@hotmail.com>
2022-04-25 11:29:05 +0100

    Added in break styles

--- js/findyourstay.js
+++ js/findyourstay.js
@@ -1,12 +1,12 @@
 document.body.onload = function () {
-    var noOfPeople = localStorage.getItem("noOfPeople");
-    var checkInDate = localStorage.getItem("checkInDate");
-    var checkOutDate = localStorage.getItem("checkOutDate");
-    var price = localStorage.getItem("price"); 
-    var style = localStorage.getItem("style");
-    console.log(noOfPeople);
-    console.log(checkInDate);
-    console.log(checkOutDate);
-    console.log(price);
-    console.log(style);
-  };
+  var noOfPeople = localStorage.getItem("noOfPeople");
+  var checkInDate = localStorage.getItem("checkInDate");
+  var checkOutDate = localStorage.getItem("checkOutDate");
+  var price = localStorage.getItem("price");
+  var style = localStorage.getItem("style");
+  console.log(noOfPeople);
+  console.log(checkInDate);
+  console.log(checkOutDate);
+  console.log(price);
+  console.log(style);
+};
