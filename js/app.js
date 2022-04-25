80965e5def623066c76c13c89450a458cf1d53d0
RebeccaFrancey <francey-r@ulster.ac.uk>
2022-04-25 11:25:42 +0100

    added stay style option to form

--- js/app.js
+++ js/app.js
@@ -5,15 +5,18 @@ formButton.onclick = function () {
   const checkInDate = document.getElementById("checkIn");
   const checkOutDate = document.getElementById("checkOut");
   const noOfPeople = document.getElementById("noOfPeople");
-  const price = document.getElementById("price");
+  const price = document.getElementById("price"); 
+  const style = document.getElementById("style");
   console.log("Check out date: " + noOfPeople.value);
   console.log("Check in date: " + checkInDate.value);
   console.log("Check out date: " + checkOutDate.value);
   console.log("price: " + price.value);
+  console.log("style: " + style.value);
   localStorage.setItem("noOfPeople", noOfPeople.value);
   localStorage.setItem("checkInDate", checkInDate.value);
   localStorage.setItem("checkOutDate", checkOutDate.value);
-  localStorage.setItem("price", price.value);
+  localStorage.setItem("price", price.value); 
+  localStorage.setItem("style", style.value);
 
   window.location.href = "./FindYourStay.html";
 };
--- js/findyourstay.js
+++ js/findyourstay.js
@@ -2,9 +2,11 @@ document.body.onload = function () {
     var noOfPeople = localStorage.getItem("noOfPeople");
     var checkInDate = localStorage.getItem("checkInDate");
     var checkOutDate = localStorage.getItem("checkOutDate");
-    var price = localStorage.getItem("price");
+    var price = localStorage.getItem("price"); 
+    var style = localStorage.getItem("style");
     console.log(noOfPeople);
     console.log(checkInDate);
     console.log(checkOutDate);
     console.log(price);
+    console.log(style);
   };
