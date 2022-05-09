adcaa34976c689544f6a446011cbf26578a16fa1
Anna McVerry <mcverry-a@ulster.ac.uk>
2022-05-09 16:11:06 +0100

    Dynamic rendering from the form

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@ console.log(formButton);
 formButton.onclick = function () {
   //   console.log("test");
   const destination = document.getElementById("destination");
-  const stayStyle = document.getElementById("stay-style");
+  const style = document.getElementById("stay-style");
   const checkInDate = document.getElementById("check-in");
   const checkOutDate = document.getElementById("check-out");
   const noOfPeople = document.getElementById("number-people");
   const price = document.getElementById("price");
   console.log("Destination " + destination.value);
-  console.log("style: " + stayStyle.value);
+  console.log("style: " + style.value);
   console.log("Check in date: " + checkInDate.value);
   console.log("Check out date: " + checkOutDate.value);
   console.log("Number of people: " + noOfPeople.value);
   console.log("price: " + price.value);
   
   localStorage.setItem("destination", destination.value);
-  localStorage.setItem("stay-style", stayStyle.value);
+  localStorage.setItem("style", style.value);
   localStorage.setItem("check-in", checkInDate.value);
   localStorage.setItem("check-out", checkOutDate.value);
   localStorage.setItem("number-people", noOfPeople.value);
--- js/findyourstay.js
+++ js/findyourstay.js
@@ -1,23 +1,23 @@
-const HOLISTAY_DATA = [
+const FAKE_HOLIDAY_DATA = [
   {
     price: "500",
     people: "2",
     style: "Bit of Both",
+    url: "./Images/galway.jpg",
   },
   {
     price: "200",
     people: "1",
     style: "City Break",
+    url: "./Images/belfast.jpg",
   },
   {
     price: "100",
     people: "3",
-    houseType: "Outdoor adventure",
+    style: "Outdoor Adventure",
+    url: "./Images/armagh.jpg",
   },
 ];
-
-
-
 
 document.body.onload = function () {
   var destination = localStorage.getItem("destination");
@@ -34,37 +34,74 @@ document.body.onload = function () {
   console.log(style);
 
   // Prints out from form on homepage
-  const anythingContainer = document.getElementById("anything");
-  const body = document.createElement("div");
+  // const anythingContainer = document.getElementById("anything");
+  // const body = document.createElement("div");
 
   // body.className can assign to a css file to make everything the same
 
-  const checkInDateText = document.createElement("p");
-  checkInDateText.innerHTML = checkInDate;
-  checkInDateText.className = "checkInDateCSS";
+  // const checkInDateText = document.createElement("p");
+  // checkInDateText.innerHTML = checkInDate;
+  // checkInDateText.className = "checkInDateCSS";
 
-  const checkOutDateText = document.createElement("p");
-  checkOutDateText.innerHTML = checkOutDate;
-  checkOutDateText.className = "checkOutDateCSS";
+  // const checkOutDateText = document.createElement("p");
+  // checkOutDateText.innerHTML = checkOutDate;
+  // checkOutDateText.className = "checkOutDateCSS";
 
-  const noOfPeopleText = document.createElement("p");
-  noOfPeopleText.innerHTML = noOfPeople;
-  noOfPeopleText.className = "noOfPeopleCSS";
+  // const noOfPeopleText = document.createElement("p");
+  // noOfPeopleText.innerHTML = noOfPeople;
+  // noOfPeopleText.className = "noOfPeopleCSS";
 
-  const priceText = document.createElement("p");
-  priceText.innerHTML = price;
-  priceText.className = "priceCSS";
-  // can move across in CSS
+  // const priceText = document.createElement("p");
+  // priceText.innerHTML = price;
+  // priceText.className = "priceCSS";
+  // // can move across in CSS
 
-  const styleText = document.createElement("p");
-  styleText.innerHTML = style;
-  styleText.className = "styleCSS";
+  //   const styleText = document.createElement("p");
+  //   styleText.innerHTML = style;
+  //   styleText.className = "styleCSS";
 
-  //Last thing you do
-  body.appendChild(checkInDateText);
-  body.appendChild(checkOutDateText);
-  body.appendChild(noOfPeopleText);
-  body.appendChild(priceText);
-  body.appendChild(styleText);
-  anythingContainer.appendChild(body);
+  //   //Last thing you do
+  //   body.appendChild(checkInDateText);
+  //   body.appendChild(checkOutDateText);
+  //   body.appendChild(noOfPeopleText);
+  //   body.appendChild(priceText);
+  //   body.appendChild(styleText);
+  //   anythingContainer.appendChild(body);
+  // };
+
+  const anythingContainer = document.getElementById("root-container");
+  const body = document.createElement("div");
+
+  showListing(); //print everything that is inside list
 };
+function showListing() {
+  const rootContianer = document.getElementById("data-container");
+  const filteredList = FAKE_HOLIDAY_DATA.filter(
+    (listing) =>
+      listing.price == localStorage.getItem("price") &&
+      listing.style == localStorage.getItem("style")
+  );
+  filteredList.forEach((listing) => {
+    //console.log(listing)
+    const createdListing = createListing(listing);
+    rootContianer.appendChild(createdListing);
+  });
+}
+
+function createListing(holidayListing) {
+  const baseListing = document.createElement("div");
+  const image = document.createElement("img");
+  image.setAttribute("src", holidayListing.url);
+  image.className = "listingImage";
+  const style = document.createElement("p");
+  style.innerHTML = "Style: " + holidayListing.style;
+  const price = document.createElement("p");
+  price.innerHTML = "Price: " + holidayListing.price;
+  const people = document.createElement("p");
+  people.innerHTML = "People: " + holidayListing.people;
+  baseListing.appendChild(image);
+  baseListing.appendChild(style);
+  baseListing.appendChild(people);
+  baseListing.appendChild(price);
+  return baseListing;
+}
